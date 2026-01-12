725f27f97ae2806c71f91a9afa62d0639beee202
chendu2017 <chen1443@e.ntu.edu.sg>
2026-01-12 14:58:19 +0000

    Deploying to gh-pages from @ chendu2017/chendu2017.github.io@7408b0a4abb08a25736506316a578185581ef5ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-talks",
           title: "Talks",
-          description: "Last Updated - 2025 August",
+          description: "Last Updated - 2025 November",
           section: "Navigation",
           handler: () => {
             window.location.href = "/talks/";
